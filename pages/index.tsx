adade241384fdb443884b93d54c6cbe3863011fb
Paul Schultz <pschultz@pobox.com>
2021-12-02 16:44:26 -0600

    Mockup of scheduler view in react

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,112 +1,51 @@
-import type { Student, StudentAction, StudentState, SelectedFile } from 'custom-types';
-import { StudentActionKind } from '@src/enums';
-import { ExportCsv, ImportCsv, StudentStack, StudentViewer } from '@src/components';
-import { useReducer, useState } from 'react';
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+const hours = [
+  '8:00-8:55',
+  '9:05-10:00',
+  '10:10-11:05',
+  '11:15-12:10',
+  '12:20-1:15',
+  '1:25-2:20',
+  '2:30-3:25',
+  '3:35-4:30'
+];
 
 /**
  * {@link Home} implements the main viewer for the webpage root
  * @returns React function component
  */
-export const Home: React.FC = () => {
-  const [csvFile, setCsvFile] = useState<SelectedFile>({ file: '', date: new Date() });
-  const [studentState, studentDispatch] = useReducer<React.Reducer<StudentState, StudentAction>>(
-    studentReducer,
-    { activeStudents: [], inactiveStudents: [] }
-  );
+export const Home: React.FC = () => (
+  <div className="h-screen w-screen p-4">
+    <h1>Scheduler - Weekly Schedule</h1>
 
-  return (
-    <div className="h-screen w-screen">
-      {!studentState.activeStudents.length && !studentState.inactiveStudents.length ? (
-        <ImportCsv studentDispatch={studentDispatch} csvFile={csvFile} setCsvFile={setCsvFile} />
-      ) : (
-        <div className="flex justify-between h-full w-full">
-          <div className="grid grid-cols-3 gap-4 h-full w-full">
-            <StudentStack students={studentState.activeStudents.slice(1, 6)} />
-            <div className="col-span-2">
-              <StudentViewer
-                student={studentState.activeStudents[0]}
-                studentDispatch={studentDispatch}
-              />
-            </div>
+    <p>Select SI's for class hours and lab hours.</p>
+
+    <form>
+      <div className="flex space-x-4 justify-between items-start border-2 border-black py-4 px-8">
+        {days.map((day) => (
+          <div key={day} className="divide-y divide-light-blue-400">
+            <p>{day}</p>
+            {hours.map((hour) => (
+              <div key={`${day}-${hour}`} className="flex justify-between">
+                <label className="px-2">{hour}</label>
+                <select name="SI" id="SI" className="border-2 border-gray">
+                  <option value="blank"></option>
+                  <option value="Student 1">Student 1</option>
+                  <option value="Student 2">Student 2</option>
+                  <option value="Student 3">Student 3</option>
+                </select>
+              </div>
+            ))}
           </div>
-          <ExportCsv studentState={studentState} csvFile={csvFile} />
-        </div>
-      )}
-    </div>
-  );
-};
+        ))}
+      </div>
+      <div>
+        <p>Click "Submit" button to confirm.</p>
+        <input type="submit" value="Submit"></input>
+      </div>
+    </form>
+  </div>
+);
 Home.displayName = 'Home';
-
-/**
- * Reducer function for useReducer hook
- * @param state - The current student state
- * @param action - The action to do on the student state
- * @returns an updated student state
- */
-const studentReducer = (state: StudentState, action: StudentAction): StudentState => {
-  const { activeStudents, inactiveStudents } = state;
-  const { type, payload } = action;
-
-  if (type === StudentActionKind.SET_STUDENTS) {
-    // Randomize the payload as active students
-    return {
-      activeStudents: shuffleArray(payload as Student[]),
-      inactiveStudents: []
-    };
-  }
-
-  // If there are no active students, return the current state
-  if (!activeStudents.length) {
-    return state;
-  }
-
-  // Get the current student and remove it from activeStudents
-  const currentStudent = activeStudents[0];
-  activeStudents.shift();
-
-  switch (type) {
-    case StudentActionKind.SKIP:
-      (currentStudent as Student).numOfSkips++;
-      break;
-    case StudentActionKind.SATISFIED:
-      (currentStudent as Student).numOfSatisfactions++;
-      break;
-
-    case StudentActionKind.DISSATISFIED:
-      (currentStudent as Student).numOfDissatisfactions++;
-      break;
-  }
-
-  // Add the current student to the end of inactiveStudents
-  inactiveStudents.push(currentStudent as Student);
-
-  // If there are no remaining active students, randomize the inactive students and set them as active students
-  if (!activeStudents.length) {
-    return {
-      activeStudents: shuffleArray(inactiveStudents),
-      inactiveStudents: []
-    };
-  }
-
-  // Return the updated student state
-  return {
-    activeStudents,
-    inactiveStudents
-  };
-};
-
-/**
- * Fisher-Yates (aka Knuth) Shuffle.
- * @param array - The array to randomize
- * @returns a randomized array
- */
-function shuffleArray<T>(array: T[]): T[] {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-
-  return array;
-}
 
 export default Home;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,3 @@
 // Organize exports alphabetically
-export * from './ExportCsv';
-export * from './ImportCsv';
 export * from './Layout';
 export * from './Meta';
-export * from './StudentStack';
-export * from './StudentViewer';
