14c59b20bef1dcfb87120ff590dfa898315be712
schultzp2020 <pschultz@pobox.com>
2021-12-17 05:07:49 +0000

    Deploying to source from @ schultzp2020/Agile-Group-3@38a5a6ec53068f1bd791b2f72ccf3de4cd8ae35d 🚀

---
+++ api/view-sis-with-conflicts.php
@@ -0,0 +1,49 @@
+<?php
+include "database.php";
+
+class SI {
+  public int $student_id;
+  public string $name;
+  public int $time;
+  public int $day;
+
+  public function __construct(int $student_id, string $name, int $time, int $day) {
+    $this->student_id = $student_id;
+    $this->name = $name;
+    $this->time = $time;
+    $this->day = $day;
+  }
+}
+
+function view_sis_with_conflicts(PDO $conn) {
+  $stmt = $conn->prepare("SELECT student.id, student.name, conflict.time, conflict.day FROM student
+    LEFT JOIN conflict ON conflict.student = student.studentid");
+
+  $stmt->execute();
+
+  $siList = array();
+
+  foreach ($stmt as $row)
+  {
+    $student_id = intval($row['student.id']);
+    $name = $row['student.name'];
+    $time = intval($row['conflict.time']);
+    $day = intval($row['conflict.day']);
+
+    $si = new SI($student_id, $name, $time, $day);
+    array_push($siList, $si);
+  }
+
+  echo json_encode($siList);
+}
+
+try {
+  $conn = connect_to_database();
+  view_sis_with_conflicts($conn);
+} catch(PDOException $e) {
+    http_response_code(500); 
+    die("{ \"success\": false, \"error\": \"" . $e->getMessage() . "\" }");
+} finally {
+  $conn = null;
+}
+?>
