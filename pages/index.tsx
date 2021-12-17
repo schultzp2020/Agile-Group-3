1fa36897d5ffc9498476f835639cfe5817addcd6
iz3k93 <isaacmoua93@gmail.com>
2021-12-16 22:51:26 -0600

    Updated Css index

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,14 @@ export const Home: React.FC = () => (
   <div className="bg-blue-800">
     <div className="bg-blue-900 p-4 flex justify-between items-center">
       <div className="flex items-center">
-        <h1 className="text-center text-3xl text-white">Home Page</h1>
+        <h1 className="text-blue-200 text-center text-3xl">Home Page</h1>
+      </div>
+      <div className="flex items-center">
+        <h1 className="font-serif text-blue-200 text-center text-6xl">Si Scheduler Tool</h1>
       </div>
       <div>
-        <h2 className="text-white">Agile Group 3</h2>
-        <p className="text-white">Welcome to our Agile Group 3 Project.</p>
+        <h2 className="text-blue-200">Agile Group 3</h2>
+        <p className="text-blue-200">Welcome to our Agile Group 3 Project.</p>
       </div>
     </div>
     <div>
