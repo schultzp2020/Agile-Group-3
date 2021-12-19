24e67588288c6cc210de7473f5d134f8c7b082b6
schultzp2020 <pschultz@pobox.com>
2021-12-19 20:25:54 +0000

    Deploying to source from @ schultzp2020/Agile-Group-3@f2d628207680f186240675e9fc265b11d3bbef85 🚀

---
+++ api/delete-course.php
@@ -0,0 +1,38 @@
+<?php
+include "database.php";
+
+function delete_course(PDO $conn, int $course_id) {
+  $stmt = $conn->prepare("DELETE FROM course
+    WHERE courseid = :courseid;");
+
+  $stmt->bindParam('courseid', $course_id, PDO::PARAM_INT);
+
+  $stmt->execute();
+}
+
+header('Content-Type: application/json;');
+
+$body = json_decode(file_get_contents('php://input'));
+
+if(!isset($body->courseId)) {
+  http_response_code(400); 
+  die('{ "success": false, "error": "A parameter is missing" }');
+}
+
+$course_id = intval($body->courseId);
+
+if (!is_int($course_id)) {
+  http_response_code(400); 
+  die('{ "success": false, "error": "Course ID is not an integer" }');
+}
+
+try {
+  $conn = connect_to_database();
+  delete_course($conn, $course_id);
+} catch(PDOException $e) {
+    http_response_code(500); 
+    die("{ \"success\": false, \"error\": \"" . $e->getMessage() . "\" }");
+} finally {
+  $conn = null;
+}
+?>
